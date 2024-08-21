d9ea02820c1797aa26b3f8759bdb819f0ec40ccd
anis-bns-017 <smanisbns@gmail.com>
2024-08-22 00:47:06 +0600

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,18 @@ import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element = {<Home />}></Route>
-        <Route path="/profile" element = {<Profile />}></Route>
-        <Route path="/about" element = {<About />}></Route>
-        <Route path="/sign-in" element = {<Signin />}></Route>
-        <Route path="/sign-up" element = {<SignUp />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
   );
